9a5656349967ab27ef97e49c94f9bf021c77bbaf
shehzi124 <shahzad2539184@gmail.com>
2023-10-25 11:18:36 +0500

    origin

---
+++ app.js
@@ -0,0 +1,141 @@
+// console.log("hello word");
+// console.log("4+4");
+
+
+
+// data types
+// string "commas ma jo bi likha ga as it is"
+// number 54.54545
+// boolen true || false 
+
+
+
+
+
+
+
+// console.log(4/4);
+// console.log(4 + "4");
+// console.log(4 +"4");
+
+
+
+
+// alert('hello word')
+// console.log('hello word');
+// console.log('hello word');
+// console.log('hello word');
+// console.log('hello word');
+// console.log('hello word');
+// console.log('hello word');
+
+
+
+
+
+
+// var names ='Abdullah';
+// let names ='Abdullah';
+// const names ='Abdullah';
+// console.log(names);
+
+
+
+// var
+// reassign == true
+// redeclare == true
+
+
+
+// let
+// reassign == true
+// redeclear == true
+
+
+
+
+
+
+// var names = 'Abdullah';
+// names = 'usman';
+// console.log(names);
+
+
+// let names = 'Abdullah';
+// names = 'usman';
+// console.log(names);
+
+
+// const names = 'Abdullah';
+// names = 'Usman'
+// console.log(names);
+
+
+
+// var names = 'abdullah';
+// console.log(names);
+
+
+
+
+
+// let username =prompt('what is your name');
+// console.log(username);
+
+
+
+
+
+// let names = "shahzad"
+// let age = "18"
+// console.log("my names is" + ' ' names  + ' '  + "and my age is" +   ' ' + age)
+
+
+
+// percentage
+
+// let userName = prompt('Enter your name');
+// let chemMarks = Number (prompt('Enter Chemistry marks'))
+// let phyMarks = Number (prompt('Enter physics marks'))
+// let mathsMarks = Number (prompt('Enter maths marks'))
+// let bioMarks = Number (prompt('Enter biology marks'))
+
+// let totalmarks = 400;
+// let usertotal = chemMarks + phyMarks + mathsMarks + bioMarks;
+// let percentage = usertotal / totalmarks * 100;
+
+
+// console.log('student name' + userName + "got" + percentage + "%");
+
+
+
+
+
+
+// let num = 1;
+// num = num + 2;
+// num += 2
+// console.log(num);
+
+
+
+// let cond = num1 > num2;
+// let cond = num1 < num2;
+// let cond = num1 === num2;
+// let cond = num1 == num2;
+// let cond = num1 != num2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
